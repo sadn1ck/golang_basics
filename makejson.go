9ef311379943bc70a0635b455692fe367e32467f
Anik Das <anikdas0811@gmail.com>
2020-06-20 08:46:05 +0530

    Added makejson

---
+++ makejson.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main3() {
+
+	mapv := make(map[string]string)
+	reader := bufio.NewReader(os.Stdin)
+	/*
+		// Alternative input method is using NewScanner
+		in = bufio.NewScanner(os.Stdin)
+		// for name
+		in.Scan()
+		name = in.Text()
+		// etc
+	*/
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter your address: ")
+	address, _ := reader.ReadString('\n')
+	// Removing newline character from input string
+	mapv["name"] = name[:len(name)-1]
+	mapv["address"] = address[:len(address)-1]
+
+	// Create JSON object from the map
+	myjson, err := json.Marshal(mapv)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(myjson))
+}
--- slice.go
+++ slice.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func main() {
+func main1() {
 	arr := make([]int, 0, 3)
 
 	var inp string
